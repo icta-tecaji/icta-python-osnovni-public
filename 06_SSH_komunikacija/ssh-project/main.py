45847dc4e7739bff4924cb02cea19da7cb54a3c3
Leon <leon.stefanic@ltfe.org>
2022-06-08 12:34:00 +0000

    add

--- 06_SSH_komunikacija/ssh-project/main.py
+++ 06_SSH_komunikacija/ssh-project/main.py
@@ -1,10 +1,23 @@
-from sshclient.remote_client import RemoteClient
+from sshclient.remote_client import RemoteClient, RemoteCommandExecutionErrorCode
 
 
 if __name__ == "__main__":
     client = RemoteClient(
-        host="212.101.140.9", port=2022, user="testuser", password="test"
+        host="212.101.140.9",
+        port=2022,
+        user="testuser",
+        password="test",
+        authentication_mode="password",
     )
-    print("working")
-    client.execute_command("nekei")
+    try:
+        results = client.execute_command("ls /", verbose=False)
+        print(results)
+        results = client.execute_commands("uname -a", "pwd", "ls /", verbose=False)
+        print(results)
+        results = client.execute_commands(["uname -a", "pwd", "ls /"], verbose=False)
+        print(results)
+        client.execute_command("nekei")
+    except RemoteCommandExecutionErrorCode as err:
+        print(err)
+
     client.disconnect()
--- 06_SSH_komunikacija/ssh-project/sshclient/remote_client.py
+++ 06_SSH_komunikacija/ssh-project/sshclient/remote_client.py
@@ -1,7 +1,13 @@
 from paramiko import SSHClient, AutoAddPolicy
+from typing import Dict, List
+from scp import SCPClient
 
 
 class BadAuthenticationModeError(Exception):
+    pass
+
+
+class RemoteCommandExecutionErrorCode(Exception):
     pass
 
 
@@ -26,6 +32,7 @@ class RemoteClient:
         self.allow_all_host_keys: bool = allow_all_host_keys
         self.authentication_mode: str = authentication_mode
         self.client: SSHClient = None
+        self.scp: SCPClient = None
 
     def __connect(self):
         """Open connection to a remote host."""
@@ -57,11 +64,36 @@ class RemoteClient:
             raise BadAuthenticationModeError(
                 "Please use password or passphrase option for authentication_mode!"
             )
+        self.scp = SCPClient(self.client.get_transport())
         return self.client
 
-    def execute_command(self, command: str):
+    def execute_command(self, command: str, verbose: bool = True) -> List[str]:
+        """Execute a single command."""
         if not self.client:
             self.client = self.__connect()
+        sdtin, sdtout, sdterr = self.client.exec_command(command)
+        status_code: int = sdtout.channel.recv_exit_status()
+        if status_code != 0:
+            print(f"--> ERROR EXECUTING COMMAND: {command}")
+            raise RemoteCommandExecutionErrorCode(
+                f"\nCommand error ->\nStatus: {status_code}\nMessage:\n{sdterr.read().decode()}"
+            )
+        stdout_text = sdtout.read().decode()
+        if verbose:
+            print(f"--> EXECUTING COMMAND: {command}")
+            print(stdout_text)
+        return [result.strip() for result in stdout_text.split("\n") if result != ""]
+
+    def execute_commands(self, *args, verbose: bool = True) -> Dict[str, List[str]]:
+        """Execute multiple commands in succession."""
+        if isinstance(args[0], list):
+            commands = args[0]
+        else:
+            commands = args
+        merged_results = dict()
+        for command in commands:
+            merged_results[command] = self.execute_command(command, verbose=verbose)
+        return merged_results
 
     def disconnect(self):
         """Close SSH connection."""
