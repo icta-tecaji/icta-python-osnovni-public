14b56c4a0cd955ffde3c65c7ad5ea7d6e8b92029
Leon <leon.stefanic@ltfe.org>
2022-06-14 08:34:51 +0000

    add

--- 06_SSH_komunikacija/ssh-project/main.py
+++ 06_SSH_komunikacija/ssh-project/main.py
@@ -20,15 +20,21 @@ if __name__ == "__main__":
     # except RemoteCommandExecutionErrorCode as err:
     #     print(err)
 
-    client.execute_command("touch ~/test.txt", verbose=True)
-    client.execute_command("touch ~/test2.txt", verbose=True)
-    client.execute_command("touch ~/test3.txt", verbose=True)
+    # client.execute_command("touch ~/test.txt", verbose=True)
+    # client.execute_command("touch ~/test2.txt", verbose=True)
+    # client.execute_command("touch ~/test3.txt", verbose=True)
 
-    client.download_files(
-        ["~/test.txt"],
-        local_save_location="/home/administrator/izobrazevanje-python-osnovni-plume/data",
+    # client.download_files(
+    #     ["~/test.txt"],
+    #     local_save_location="/home/administrator/izobrazevanje-python-osnovni-plume/data",
+    # )
+    # client.download_files(["~/test2.txt", "~/test3.txt"])
+
+    # client.download_folder("/home/testuser/test")
+    client.upload_files(
+        ["remote_client.py", "main.py"], remote_save_location="~/nova_mapa"
     )
-    client.download_files(["~/test2.txt"])
-    client.download_files(["~/test3.txt"])
-
+    client.upload_folder("sshclient")
+    client.execute_command("ls ~", verbose=True)
+    client.execute_command("ls ~/nova_mapa", verbose=True)
     client.disconnect()
--- 06_SSH_komunikacija/ssh-project/sshclient/remote_client.py
+++ 06_SSH_komunikacija/ssh-project/sshclient/remote_client.py
@@ -110,31 +110,78 @@ class RemoteClient:
             self.client = self.__connect()
         try:
             self.scp.get(remote_path, local_path)
-        except SCPException:
-            print(f"  -> File missing. Skipping downloading file {remote_path}.")
+        except SCPException as err:
+            print(
+                f"  -> File missing. Skipping downloading file {remote_path}. Describtion: {err}"
+            )
             self.client = None
 
     def download_files(
         self, remote_paths: List[str], local_save_location: str = DEFAULT_PATH
     ) -> None:
         os.makedirs(local_save_location, exist_ok=True)
+        # TODO June 08, 2022: dodaj barve in lepši prikaz
+        # TODO June 14, 2022: dodaj grab za remote file
         for remote_path in remote_paths:
             remote_file_name = Path(remote_path).name
             local_path = os.path.join(local_save_location, remote_file_name)
             print(f"Downloading file: {remote_file_name} to local folder: {local_path}")
             self._download_file(remote_path, local_path)
 
-    def download_folder(self):
-        pass
+    def download_folder(
+        self, remote_folder_path: str, local_save_location: str = DEFAULT_PATH
+    ) -> None:
+        os.makedirs(local_save_location, exist_ok=True)
+        if not self.client:
+            self.client = self.__connect()
+        try:
+            print(
+                f"Downloading folder: {remote_folder_path} to local folder: {local_save_location}"
+            )
+            self.scp.get(remote_folder_path, local_save_location, recursive=True)
+        except SCPException as err:
+            print(f"Error while downloading folder. Describtion: {err}")
+            self.client = None
 
-    def _upload_file(self):
-        pass
+    def _upload_file(self, local_path: str, remote_path: str) -> None:
+        """Upload a single file to a remote host."""
+        if not self.client:
+            self.client = self.__connect()
+        try:
+            self.scp.put(local_path, remote_path=remote_path)
+        except SCPException as err:
+            print(
+                f"  -> Error uploading. Skipping uploading file {local_path}. Describtion: {err}"
+            )
+            self.client = None
 
-    def upload_files(self):
-        pass
+    def upload_files(
+        self, local_paths: List[str], remote_save_location: str = "~"
+    ) -> None:
+        self.execute_commands(f"mkdir -p {remote_save_location}")
+        for local_path in local_paths:
+            if os.path.isfile(local_path):
+                local_file_name = Path(local_path).name
+                remote_path = os.path.join(remote_save_location, local_file_name)
+                print(
+                    f"Uploading file: {local_file_name} to remote folder: {remote_path}"
+                )
+                self._upload_file(local_path, remote_path)
+            else:
+                print(f"  -> File missing. Skipping uploading file {local_path}.")
 
-    def upload_folder(self):
-        pass
+    def upload_folder(self, local_folder_path: str, remote_save_location: str = "~"):
+        self.execute_commands(f"mkdir -p {remote_save_location}")
+        if not self.client:
+            self.client = self.__connect()
+        try:
+            print(
+                f"Uploading folder: {local_folder_path} to remote folder: {remote_save_location}"
+            )
+            self.scp.put(local_folder_path, remote_save_location, recursive=True)
+        except SCPException as err:
+            print(f"Error while uploading folder. Describtion: {err}")
+            self.client = None
 
     def disconnect(self):
         """Close SSH connection."""
