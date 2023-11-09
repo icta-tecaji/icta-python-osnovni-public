eef3ff8e9f89e56508f7c2208c27994c885a46ec
Leon Stefanic Juznic <leon.stefanic@ltfe.org>
2023-11-09 07:23:11 +0100

    Fix print statements and add countdown function

---
+++ 07_Izpis_spremenljivke_osnovni_podatkovni_tipi/exercises/counter.py
@@ -0,0 +1,9 @@
+import time
+
+print("3", end="...")
+time.sleep(1)
+print("2", end="...")
+time.sleep(1)
+print("1", end="...")
+time.sleep(1)
+print("Go!")
---
+++ 07_Izpis_spremenljivke_osnovni_podatkovni_tipi/exercises/counter_flush.py
@@ -0,0 +1,9 @@
+import time
+
+print("3", end="...", flush=True)
+time.sleep(1)
+print("2", end="...", flush=True)
+time.sleep(1)
+print("1", end="...", flush=True)
+time.sleep(1)
+print("Go!", flush=True)
